36579803bc1c80a6910a5a323f527b7daf467bb8
Trang Luu <tluu9@mail.greenriver.edu>
2019-05-13 17:04:14 -0400

    Added survey route

--- index.php
+++ index.php
@@ -6,11 +6,14 @@ error_reporting(E_ALL);
 
 //require the autoload file autoload.php
 require_once('vendor/autoload.php');
+require_once('model/validation-functions.php');
 
 
 //Create an instance of the Base class/ instantiate Fat-Free
 $f3 = Base::instance();
 
+//Array
+$f3 -> set('theAnswers', array('This midterm is easy','I like Midterm','Today is Monday'));
 
 //Turn on Fat-free error reporting/Debugging
 $f3->set('DEBUG',3);
@@ -19,7 +22,34 @@ $f3->set('DEBUG',3);
 $f3->route('GET /', function()
 {
     echo"<h1>Midterm Survey</h1>
-    <br><p><a href='survey'>Take My Midterm Survey</a></p>";
+    <br><p><a href='form'>Take My Midterm Survey</a></p>";
+});
+
+$f3->route('GET|POST /form',
+    function ($f3)
+    {
+
+        if (!empty($_POST)) {
+            $answers = $_POST['answers'];
+
+            $f3->set('answers', $answers);
+
+            if (form()) {
+                $_SESSION['answers'] = $answers;
+
+                $f3->reroute('/summary');
+            }
+
+        }
+        $view=new Template();
+        echo $view->render( 'views/form.html');
+    });
+
+//summary
+$f3->route('GET|POST /summary', function() {
+
+    $view = new Template();
+    echo $view->render('views/results.html');
 });
 
 //Run fat free F3
